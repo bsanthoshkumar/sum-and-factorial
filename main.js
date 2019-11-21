0576b070cdaf173346bc1feaf7ea0b381341e009
santhosh <santhosh.kumar@thoughtworks.com>
2019-11-21 11:37:16 +0530

    modified main(required chalk)

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const sum = require("./src/sumAndFactorial").sum;
 const factorial = require("./src/sumAndFactorial").factorial;
+const chalk = require("chalk");
 
 const main = function() {
   const a = 5;
   const b = 3;
-  console.log("sum of " + a + " and " + b + " is " + sum(a, b));
-  console.log("factorial of " + a + " is " + factorial(a));
+  console.log(
+    "sum of " + a + " and " + b + " is " + chalk.redBright(sum(a, b))
+  );
+  console.log("factorial of " + a + " is " + chalk.greenBright(factorial(a)));
 };
 
 main();
