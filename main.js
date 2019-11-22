5d6b8a0a1b5efb065b2fdba14409cedcc7f980cc
santhosh <santhosh.kumar@thoughtworks.com>
2019-11-22 11:11:23 +0530

    added chalk colors to main.js and testsum.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const validateArgs = require("./src/sumAndFactorial").validateArgs;
 const chalk = require("chalk");
 
 const main = function(args) {
-  console.log(validateArgs(args.slice(2)));
+  console.log(chalk.greenBright(validateArgs(args.slice(2))));
 };
 
 main(process.argv);
--- test/testsum.js
+++ test/testsum.js
@@ -3,33 +3,43 @@ const factorial = require("../src/sumAndFactorial").factorial;
 const isNumber = require("../src/sumAndFactorial").isNumber;
 const validateArgs = require("../src/sumAndFactorial").validateArgs;
 const assert = require("assert");
+const chalk = require("chalk");
 
 describe("sum", function() {
-  it("should return sum of positive integers", function() {
+  it(chalk.redBright("should return sum of positive integers"), function() {
     assert.strictEqual(sum(3, 5), 8);
   });
-  it("should return sum of negative integers", function() {
+  it(chalk.redBright("should return sum of negative integers"), function() {
     assert.strictEqual(sum(-3, -5), -8);
   });
 });
 
 describe("factorial", function() {
-  it("should return factorial of given number", function() {
+  it(chalk.greenBright("should return factorial of given number"), function() {
     assert.strictEqual(factorial(5), 120);
   });
 });
 
 describe("validateArgs", function() {
-  it("should perform sum operation on given values", function() {
-    assert.strictEqual(validateArgs(["sum", "8", "17"]), 25);
-  });
+  it(
+    chalk.yellowBright("should perform sum operation on given values"),
+    function() {
+      assert.strictEqual(validateArgs(["sum", "8", "17"]), 25);
+    }
+  );
 
-  it("should perform factorial operation on given values", function() {
-    assert.strictEqual(validateArgs(["factorial", "6"]), 720);
-  });
+  it(
+    chalk.yellowBright("should perform factorial operation on given values"),
+    function() {
+      assert.strictEqual(validateArgs(["factorial", "6"]), 720);
+    }
+  );
 
-  it("should return usage for invalid arguments", function() {
-    const usage = "usage: node main.js [sum|factorial] [0-9]* [0-9]*";
-    assert.strictEqual(validateArgs(["sum", "5"]), usage);
-  });
+  it(
+    chalk.yellowBright("should return usage for invalid arguments"),
+    function() {
+      const usage = "usage: node main.js [sum|factorial] [0-9]* [0-9]*";
+      assert.strictEqual(validateArgs(["sum", "5"]), usage);
+    }
+  );
 });
